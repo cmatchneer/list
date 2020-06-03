dad548f61ccc5be36dab1c5450e38232734434fb
cmatchneer <cmatchneer@yahoo.com>
2020-06-03 19:33:07 -0400

    title and author update routes up and running

--- controllers/bookListController.js
+++ controllers/bookListController.js
@@ -12,5 +12,46 @@ module.exports={
         }).then(function(response){
             res.json(response);
         })
+    },
+    allBooksInTheList:function(req,res){
+        var id =req.params.id;
+        db.ListType.findAll({
+            where:{id:id},
+            include:[
+                db.Books
+            ]
+        }).then(function(response){
+            res.json(response);
+        })
+    },
+    finishedBook:function(req,res){
+        var id = req.params.id;
+        db.Books.update({
+            finished:true
+        },{
+            where:{id:id}
+        }).then(function(response){
+            res.json(response);
+        })
+    },
+    titleUpdate:function(req,res){
+        var id = req.body.id
+        db.Books.update({
+            title:req.body.title
+        },{
+            where:{id:id}
+        }).then(function(response){
+        res.json(response);
+        })
+    },
+    authorUpdate:function(req,res){
+        var id = req.body.id
+        db.Books.update({
+            author:req.body.author
+        },{
+            where:{id:id}
+        }).then(function(response){
+        res.json(response);
+        })
     }
 }
--- routes/api/booksList.js
+++ routes/api/booksList.js
@@ -4,6 +4,13 @@ const bookController = require("../../controllers/bookListController");
 router.route("/")
     .post(bookController.addToBookList)
 router.route("/:id")
+    .get(bookController.allBooksInTheList)
     .delete(bookController.removeBookFromList);
+router.route("/purchased/:id")
+    .put(bookController.finishedBook);
+router.route("/title")
+    .put(bookController.titleUpdate);
+router.route("/author")
+    .put(bookController.authorUpdate);
 
 module.exports = router;
